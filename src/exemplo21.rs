5bb0a1f29b5f96e194211e193b64110d92b8c404
Walter Jonas <jonasdowalter@gmail.com>
2021-10-01 19:27:24 -0400

    💪Walter Jonas
    💪Walter Jonas

---
+++ src/exemplo21.rs
@@ -0,0 +1,16 @@
+//Walter Jonas
+//Operadores de deslocamento - Deslocando Bits a direita
+//Questão 21
+
+fn main() {
+    let x: u8 = 128;
+    println!("Operador de deslocamento - Bits a direita\nValor de x: {:0>8b}", x);
+    println!("x >> 0 = {:0>8b}", x >> 0);//Não deslocado
+    println!("x >> 1 = {:0>8b}", x >> 1);
+    println!("x >> 2 = {:0>8b}", x >> 2);
+    println!("x >> 3 = {:0>8b}", x >> 3);
+    println!("x >> 4 = {:0>8b}", x >> 4);
+    println!("x >> 5 = {:0>8b}", x >> 5);
+    println!("x >> 6 = {:0>8b}", x >> 6);
+    println!("x >> 7 = {:0>8b}", x >> 7);
+}
