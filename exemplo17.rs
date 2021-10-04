8d6d2d988c8a2de792fdd402491bf47543e1d6cd
Waleson Melo <walesonmelo23@gmail.com>
2021-10-03 22:52:54 -0400

    Create exemplo17.rs

---
+++ exemplo17.rs
@@ -0,0 +1,20 @@
+fn main(){
+    let a:u16 = 0x6DB7;
+    let b:u16;
+    let c:u16;
+    let mask:u16 = 0x4;
+
+    b = a ^ mask;    
+    println!("(a) » 0x{:x} ", a);
+    println!("(mask) » 0x{:x} ", mask);
+    println!("b = a & mask ");
+    println!("b = 0x{:x} ", b);
+
+    println!("");
+
+    c = b ^ mask;    
+    println!("(b) » 0x{:x} ", b);
+    println!("(mask) » 0x{:x} ", mask);
+    println!("c = b & mask ");
+    println!("c = 0x{:x} ", c);
+}
