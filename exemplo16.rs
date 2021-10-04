1c6cc8555b78f721187ca1935a7fc397a29c8a5d
Waleson Melo <walesonmelo23@gmail.com>
2021-10-03 22:52:51 -0400

    Create exemplo16.rs

---
+++ exemplo16.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let a:u16 = 0x6DB7;
+    let b:u16;
+    let mask:u16 = 0xFF00;
+
+    b = a ^ mask;    
+    println!("(a) » 0x{:x} ", a);
+    println!("(mask) » 0x{:x} ", mask);
+    println!("b = a & mask ");
+    println!("b = 0x{:x} ", b);
+}
