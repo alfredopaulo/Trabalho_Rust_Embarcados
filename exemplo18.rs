a76ba3fffb08ed97b53f139110c774849f8b192c
Waleson Melo <walesonmelo23@gmail.com>
2021-10-03 23:04:34 -0400

    :poultry_leg: Waleson Melo

--- exemplo18.rs
+++ exemplo18.rs
@@ -2,7 +2,7 @@ fn main(){
     let mut b:u16 = 0xAF;
     let m1:u16 = 0xFC;
     let m2:u16 = 0x02;
-   
+
     println!("(b) » 0x{:x} ", b);
     println!("(M1) » 0x{:x} ", m1);
     println!("(M2) » 0x{:x} ", m2);
